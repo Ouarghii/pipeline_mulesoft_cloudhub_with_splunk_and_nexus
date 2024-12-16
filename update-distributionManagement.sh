69ad810ca374e92c5d7f55cbdbfa3395f9cf033e
Ouarghii <raslenouarghi2018@gmail.com>
2024-12-16 14:57:19 +0000

    Update Distribution Management to use JFrog repository

--- update-distributionManagement.sh
+++ update-distributionManagement.sh
@@ -38,3 +38,8 @@ git checkout -b developer || git checkout developer
 git add pom.xml update-distributionManagement.sh
 git commit -m "Update Distribution Management to use JFrog repository"
 git push dev developer
+sed -i "/<distributionManagement>/,/<\/distributionManagement>/c\  <distributionManagement>\\n    <repository>\\n      <id>jfrog</id>\\n      <url>https://evident-robust-turtle.ngrok-free.app/artifactory/mule4-jars-pom/</url>\\n    </repository>\\n  </distributionManagement>" pom.xml
+git checkout -b developer || git checkout developer
+git add pom.xml update-distributionManagement.sh
+git commit -m "Update Distribution Management to use JFrog repository"
+git push dev developer
