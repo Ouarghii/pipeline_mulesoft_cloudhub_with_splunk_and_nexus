9ff0af9ac12a0a3f5e34f8b20a06d218e6027715
Ouarghii <raslen.ouarghi@ensi-uma.tn>
2024-12-18 15:36:53 +0000

    Update Settings File to enable Hosting to Nexus

--- update-settings.sh
+++ update-settings.sh
@@ -83,3 +83,8 @@ sed -i "/<servers>/a \<server>\n    <id>jfrog</id>\n    <username>admin</usernam
   git add update-settings.sh
   git commit -m "Update Settings File to enable Hosting to Nexus"
   git push dev developer
+sed -i "/<servers>/a \<server>\n    <id>jfrog</id>\n    <username></username>\n    <password></password>\n  </server>" settings.xml
+  git add settings.xml
+  git add update-settings.sh
+  git commit -m "Update Settings File to enable Hosting to Nexus"
+  git push dev developer
