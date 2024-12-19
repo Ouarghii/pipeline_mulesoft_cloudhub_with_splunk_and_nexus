8fd32eb87aa1162392538213b6eea273c78eff42
Ouarghii <raslen.ouarghi@ensi-uma.tn>
2024-12-19 14:31:49 +0000

    Create and update Parent POM file

--- update-parentPOM.sh
+++ update-parentPOM.sh
@@ -4,7 +4,7 @@ if [ -f settings.xml ]; then
     ANYPOINT_CLIENT_SECRET_ENCRYPTED=$(sed -n "/<id>anypoint-repo<\/id>/,/<\/server>/ s/.*<password>\(.*\)<\/password>.*/\1/p" settings.xml)
     ANYPOINT_ID_DECRYPTED=$(echo "${ANYPOINT_CLIENT_ID_ENCRYPTED}" | openssl enc -aes-256-cbc -d -salt -pbkdf2 -k "wQf9vaGtyBckXAqzNWbNuC50VlgY50fOj2IF2Rn2NHA=" -base64)
     ANYPOINT_SECRET_DECRYPTED=$(echo "${ANYPOINT_CLIENT_SECRET_ENCRYPTED}" | openssl enc -aes-256-cbc -d -salt -pbkdf2 -k "wQf9vaGtyBckXAqzNWbNuC50VlgY50fOj2IF2Rn2NHA=" -base64)
-    org_id=$(curl -X GET "https://anypoint.mulesoft.com/accounts/api/me" -H "Authorization: Bearer 0e583c43-68b8-4e85-8787-c838d236f277" -H "Content-Type: application/json" | jq -r ".user.organization.id")
+    org_id=$(curl -X GET "https://anypoint.mulesoft.com/accounts/api/me" -H "Authorization: Bearer 8a254ead-e45f-4711-b2d9-d2b9800675b2" -H "Content-Type: application/json" | jq -r ".user.organization.id")
     MULE_APP_NAME=$(grep -oPm1 "(?<=<name>)[^<]+" pom.xml)
     echo "Deploying application: ${MULE_APP_NAME}"
     MULE_RUNTIME_VERSION=$(grep -oPm1 "(?<=<app.runtime>)[^<]+" pom.xml)
